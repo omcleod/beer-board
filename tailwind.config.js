a599d1d3eecef485a49bdec238efdbc0b04a78fa
Oliver McLeod <oliverwilliammcleod@gmail.com>
2022-09-20 16:19:32 +0100

    Added tailwind css and made it available to vue components.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/js/components/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
