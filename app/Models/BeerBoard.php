8299761208f83e12bc00528c11af11d298719c21
Oliver McLeod <oliverwilliammcleod@gmail.com>
2022-09-20 14:43:19 +0100

    Added eloquent relationship.

--- app/Models/BeerBoard.php
+++ app/Models/BeerBoard.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Beer;
 
 class BeerBoard extends Model
 {
@@ -12,4 +13,9 @@ class BeerBoard extends Model
     public $table = "board";
 
     protected $fillable = ['beer_id'];
+
+    public function beer() 
+    {
+        return $this->belongsTo(Beer::class, 'beer_id');
+    }
 }
