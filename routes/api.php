f363b36cc2b960ee64b8c012a1aea5c05da5096f
Oliver McLeod <oliverwilliammcleod@gmail.com>
2022-09-22 13:10:10 +0100

    Replaced api route with a resource so I can add CRUD functionality for the beer board.

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/beers', [BeerBoardController::class, 'index']);
+Route::apiResource('beers', 'App\Http\Controllers\Api\BeerBoardController');
