2212e96313b6e76dc7b47fd1522c4850e1fbf910
Oliver McLeod <oliverwilliammcleod@gmail.com>
2022-09-28 13:49:34 +0100

    Added form to add a beer to the beer board and two methods to populate the select input and and one to save the beer.

--- resources/js/components/beer-board/BeerBoardIndex.vue
+++ resources/js/components/beer-board/BeerBoardIndex.vue
@@ -1,52 +1,101 @@
 <template>
     <div>
-        <table class="border-collapse table-auto w-full text-sm">
+        <h3 class="mb-5">Add a beer to board</h3>
+        <div v-if="errors">
+            <div v-for="(v, k) in errors" :key="k" class="px-4 py-2 pr-0 mb-4 font-bold text-white bg-red-500 rounded shadow-lg">
+                <p v-for="error in v" :key="error" class="text-sm">
+                {{ error }}
+                </p>
+            </div>
+        </div>
+        <form class="mb-5 space-y-6" @submit.prevent="saveBeer">
+            <div class="space-y-4 rounded-md shadow-sm">
+                <div>
+                    <v-select :options="options" v-model="form.beer">
+                    <template slot="option" slot-scope="option">
+                        <div class="d-center">
+                            {{ option.id }}
+                        </div>
+                    </template>
+                </v-select>
+                </div>
+                <div>
+                    <label for="name" class="block text-sm font-medium text-gray-700">Price</label>
+                    <div class="mt-1">
+                        <input type="number" name="price" id="price"
+                            min="0.00" max="50.00" step="0.05"
+                            class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                            v-model="form.price">
+                    </div>
+                </div>
+            </div>
+
+            <button type="submit"
+                    class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out bg-gray-800 border border-transparent rounded-md ring-gray-300 hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring disabled:opacity-25">
+                Create
+            </button>
+        </form>
+        
+        <table class="w-full text-sm border-collapse table-auto">
             <thead>
             <tr>
-                <th class="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Name</th>
-                <th class="border-b dark:border-slate-600 font-medium p-4 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Style</th>
-                <th class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">ABV</th>
-                <th class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Brewery</th>
-                <th class="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Price</th>
+                <th class="p-4 pt-0 pb-3 pl-8 font-medium text-left border-b dark:border-slate-600 text-slate-400 dark:text-slate-200">Name</th>
+                <th class="p-4 pt-0 pb-3 font-medium text-left border-b dark:border-slate-600 text-slate-400 dark:text-slate-200">Style</th>
+                <th class="p-4 pt-0 pb-3 pr-8 font-medium text-left border-b dark:border-slate-600 text-slate-400 dark:text-slate-200">ABV</th>
+                <th class="p-4 pt-0 pb-3 pr-8 font-medium text-left border-b dark:border-slate-600 text-slate-400 dark:text-slate-200">Brewery</th>
+                <th class="p-4 pt-0 pb-3 pr-8 font-medium text-left border-b dark:border-slate-600 text-slate-400 dark:text-slate-200">Price</th>
             </tr>
             </thead>
             <tbody class="bg-white dark:bg-slate-800">
             <tr v-for="beer in beers" :key="beer.id">
-                <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+                <td class="p-4 pl-8 border-b border-slate-100 dark:border-slate-700 text-slate-500 dark:text-slate-400">
                     {{ beer.name }}
                 </td>
-                <td class="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400">
+                <td class="p-4 border-b border-slate-100 dark:border-slate-700 text-slate-500 dark:text-slate-400">
                     {{ beer.style }}
                 </td>
-                <td class="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400">
+                <td class="p-4 pr-8 border-b border-slate-100 dark:border-slate-700 text-slate-500 dark:text-slate-400">
                     {{ beer.abv }}
                 </td>
-                <td class="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400">
+                <td class="p-4 pr-8 border-b border-slate-100 dark:border-slate-700 text-slate-500 dark:text-slate-400">
                     {{ beer.brewery }}
                 </td>
-                <td class="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400">
+                <td class="p-4 pr-8 border-b border-slate-100 dark:border-slate-700 text-slate-500 dark:text-slate-400">
                     &pound;{{ beer.price }}
                 </td>
                 <td class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">
                     <button @click="deleteBeer(beer.id)"
-                            class="inline-flex items-center px-4 py-2 bg-red-500 rounded-md font-semibold text-xs text-white uppercase">
+                            class="inline-flex items-center px-4 py-2 text-xs font-semibold text-white uppercase bg-red-500 rounded-md">
                             Delete
                     </button>
                 </td>
-            </tr>
+            </tr> 
             </tbody>
         </table>
     </div>
 </template>
 <script>
     import useBeerBoard from '../../composables/beer-board';
-    import { onMounted, useMounted } from "vue";
+    import { onMounted, reactive, useMounted } from "vue";
     
     export default {
         setup() {
-            const { beers, getBeers, destroyBeer } = useBeerBoard()
+            const form = reactive({
+                'beer': '',
+                'price': '',
+            })
+            
+            const { beers, options, errors, getBeers, destroyBeer, getBeerNames, storeBeer } = useBeerBoard()
 
-            onMounted(getBeers)
+            onMounted(() => {
+                getBeers()
+                getBeerNames()
+            })
+
+            const saveBeer = async () => {
+                await storeBeer({...form});
+                await getBeers();
+            }
 
             const deleteBeer = async (id) => {
                 //TODO Replace with modal
@@ -60,6 +109,10 @@
 
             return {
                 beers,
+                form,
+                errors,
+                options,
+                saveBeer,
                 deleteBeer
             }
         }
