6159f1564a476cb922393e16b0fd8a9731774717
Oliver McLeod <oliverwilliammcleod@gmail.com>
2022-09-28 13:33:14 +0100

    Created request to validate beer board data.

---
+++ app/Http/Requests/BeerBoardRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class BeerBoardRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'beer_id.unique' => 'This beer already exists on the beer board.',
+        ];
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'beer_id' => 'required|unique:board',
+            'price' => 'required|numeric|between:0,49.99',
+        ];
+    }
+}
