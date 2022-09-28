1b7f20193567dadbb5868d9c707acba84bb55489
Oliver McLeod <oliverwilliammcleod@gmail.com>
2022-09-28 13:35:52 +0100

    Added create method to BeerBoardController.

--- app/Http/Controllers/Api/BeerBoardController.php
+++ app/Http/Controllers/Api/BeerBoardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\BeerBoard;
 use App\Http\Resources\BeerBoardResource;
+use App\Http\Requests\BeerBoardRequest;
 
 class BeerBoardController extends Controller
 {
@@ -13,9 +14,17 @@ class BeerBoardController extends Controller
         return BeerBoardResource::collection(BeerBoard::all());
     }
 
-    public function destroy(BeerBoard $beer) {
+    public function destroy(BeerBoard $beer) 
+    {
         $beer->delete();
 
         return response()->noContent();
+    }
+
+    public function store(BeerBoardRequest $request)
+    {
+        $beer = BeerBoard::create($request->validated());
+
+        return new BeerBoardResource($beer);
     }
 }
